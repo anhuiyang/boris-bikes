dd30b6bca3d11d77526f40609d5d9e815dc6d282
Hui An Yang <hecatie36@hotmail.com>
2018-11-30 16:49:27 +0000

    CH15 SRP

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,25 @@
 require_relative './bike'
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
+
 
   def release_bike
-    fail "no bike" unless @bike != nil
-    @bike
+    fail "no bike" if @bikes.empty?
+    @bikes.pop
   end
 
   def dock(bike)
-    fail "no capacity" unless @bike == nil
-    @bike = bike
+    fail "no capacity" if full?
+    @bikes << bike
   end
 
+  private
+  def full?
+    @bikes.length >= 20
+  end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,16 +3,14 @@ require 'docking_station'
 describe DockingStation do
   bike = Bike.new
 
-  it { is_expected.to respond_to(:bike)}
-
-
+  it { is_expected.to respond_to(:bikes)}
 
   it 'returns docked bike' do
     subject.dock(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.bikes).to eq [bike]
   end
 
-  describe '#release_bike'
+  describe '#release_bike' do
     it 'raises error when no bike' do
       expect {subject.release_bike}.to raise_error "no bike"
     end
@@ -21,12 +19,14 @@ describe DockingStation do
       subject.release_bike
       expect(bike.working?).to be true
     end
+  end
+
   describe '#dock' do
     it 'dock bike' do
-      expect(subject.dock(bike)).to eq bike
+      expect(subject.dock(bike)).to eq [bike]
     end
     it 'raises error when capacity full' do
-      subject.dock(bike)
+      20.times{subject.dock(bike)}
       expect {subject.dock(bike)}.to raise_error "no capacity"
     end
   end
